739a966c8d975f6614309351d729f44e05eb55e4
lucas_inestal <lucasainestal@gmail.com>
2020-09-14 15:33:57 +0200

    branch setup

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+//Development
+
+
 app.listen(port, () => {
     console.log(`App is listening at http://localhost:${port}`)
 })
