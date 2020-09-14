20383eabc3c6b6d79378c3f83b201fcf6674c6ba
lucas_inestal <lucasainestal@gmail.com>
2020-09-14 15:27:04 +0200

    env variable, splitting up test and dev Collections

--- database/db.js
+++ database/db.js
@@ -1,19 +1,43 @@
 var Datastore = require("nedb");
 
 db = {};
-db.users = new Datastore({
-    filename: __dirname + "/development/users.db",
-    autoload: true,
-  });
-  db.products = new Datastore({
-    filename: __dirname + "/development/products.db",
-    autoload: true,
-  });
-  db.carts = new Datastore({
-    filename: __dirname + "/development/carts.db",
-    autoload: true,
-  });
-  db.orders = new Datastore({
-    filename: __dirname + "/development/orders.db",
-    autoload: true,
-  });
+switch (process.env.ENVIRONMENT) {
+  case "development":
+    db.users = new Datastore({
+      filename: __dirname + "/development/users.db",
+      autoload: true,
+    });
+    db.products = new Datastore({
+      filename: __dirname + "/development/products.db",
+      autoload: true,
+    });
+    db.carts = new Datastore({
+      filename: __dirname + "/development/carts.db",
+      autoload: true,
+    });
+    db.orders = new Datastore({
+        filename: __dirname + "/development/orders.db",
+        autoload: true,
+      });
+    break;
+  case "test":
+    db.users = new Datastore({
+        filename: __dirname + "/test/users.db",
+        autoload: true,
+      });
+      db.products = new Datastore({
+        filename: __dirname + "/test/products.db",
+        autoload: true,
+      });
+      db.carts = new Datastore({
+        filename: __dirname + "/test/carts.db",
+        autoload: true,
+      });
+      db.orders = new Datastore({
+          filename: __dirname + "/test/orders.db",
+          autoload: true,
+        });
+    break;
+}
+
+module.exports = db;
